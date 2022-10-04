bfca8fd338316ed41fa586bd68235b9db66886ed
Demir Boraj <demirboraj97@gmail.com>
2022-10-04 17:42:39 +0200

    first commit

---
+++ src/main/java/com/hmdb/hmdb/HmdbApplication.java
@@ -0,0 +1,14 @@
+package com.hmdb.hmdb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HmdbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HmdbApplication.class, args);
+		System.out.println("I'm Demir, a.k.a Redi!");
+	}
+
+}
